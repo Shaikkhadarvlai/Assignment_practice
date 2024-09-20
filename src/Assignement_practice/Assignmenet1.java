e70b7c295c4b71a20ec9d4923a1f59c29d323347
Shaik. Khadar vali <khadarsk414@gmail.com>
2024-09-20 22:27:34 +0530

    first commit

---
+++ src/Assignement_practice/Assignmenet1.java
@@ -0,0 +1,31 @@
+package Assignement_practice;
+
+public class Assignmenet1 {
+
+	public static void main(String[] args)
+	{
+		
+
+		 // Initialize the variables
+       int a = 10;
+       int b = 20;
+       int c = 30;
+
+       // Perform some operations
+       int sum = a + b + c;
+       int product = a * b * c;
+       int average = (a + b + c) / 3;
+
+       // Print the results
+       System.out.println("a = " + a);
+       System.out.println("b = " + b);
+       System.out.println("c = " + c);
+       System.out.println("Sum = " + sum);
+       System.out.println("Product = " + product);
+       System.out.println("Average = " + average);
+		
+		
+
+	}
+
+}
